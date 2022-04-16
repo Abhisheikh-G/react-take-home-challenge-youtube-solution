53c88ce76bb07250aa502bd4b38972cf8a845c2d
abhisheikh <abhisheikhg@gmail.com>
2022-04-16 19:29:34 -0400

    Add dependencies and types

---
+++ src/@types/index.d.ts
@@ -0,0 +1,18 @@
+export interface IItem {
+  id: string;
+  name: string;
+  description: string;
+  variants: [IVariant];
+  imageUrls: [
+    {
+      url: string;
+    }
+  ];
+}
+
+export interface IVariant {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
